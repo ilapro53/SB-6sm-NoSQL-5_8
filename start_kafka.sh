6e364c03af20f1339d39bba44eb2e0f5b9ed97cf
Ilya Pro <ilapro53@gmail.com>
2024-04-20 02:51:20 +0300

    Update start_kafka.sh

--- start_kafka.sh
+++ start_kafka.sh
@@ -1,1 +1,4 @@
+./bin/kafka-storage format \
+    --config ./etc/kafka/kraft/server.properties \
+    --cluster-id $(./bin/kafka-storage random-uuid)
 ./bin/kafka-server-start ./etc/kafka/kraft/server.properties
